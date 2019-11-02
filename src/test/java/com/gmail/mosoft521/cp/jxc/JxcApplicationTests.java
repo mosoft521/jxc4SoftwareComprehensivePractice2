002471ce2135c5eb94714485ab96b0c639ffc7bb
Alvin Zhang <mosoft521@gmail.com>
2019-11-02 11:08:07 +0800

    init2

---
+++ src/test/java/com/gmail/mosoft521/cp/jxc/JxcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.mosoft521.cp.jxc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
